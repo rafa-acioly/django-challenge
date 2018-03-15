34be23914206faf4b6d67c4e28dc1d0f8af5ac13
Rafael Acioly <aciolyr@gmail.com>
2018-03-15 15:07:13 -0300

    Inclui validação para que emails sejam unicos
    Impede que mais de um funcionário seja registrado com o mesmo email.

--- luizalabs_employee_manager/serializers.py
+++ luizalabs_employee_manager/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import routers, serializers, viewsets
 from api.models import Employee
+from rest_framework.validators import UniqueValidator
 
 
 class EmployeeSerialize(serializers.HyperlinkedModelSerializer):
     read_only_fields = ('created_at')
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=Employee.objects.all())])
 
     class Meta:
         model = Employee
