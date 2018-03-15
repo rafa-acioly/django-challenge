486833d3c1c70d171340f6f166a2572110e688e9
Rafael Acioly <aciolyr@gmail.com>
2018-03-15 15:18:27 -0300

    Arquivo de teste movido para sua propria pasta

--- tests/employee_tests.py
+++ tests/employee_tests.py
@@ -40,10 +40,21 @@ def test_blank_email(client, temporary_employee):
     assert response.status_code == 400
 
 @pytest.mark.django_db
+def test_unique_email(client, temporary_employee):
+    from api.models import Employee
+
+    response = client.post('/employee/{}'.format(temporary_employee.id),
+                            json.dumps({"name": "Funcionado email repetido", "department": "TI", "email": "meu_email@gmail.com"}),
+                            HTTP_X_METHOD_OVERRIDE='POST',
+                            content_type='application/json')
+    
+    assert response.status_code == 400
+
+@pytest.mark.django_db
 def test_update(client, temporary_employee):
     from api.models import Employee
 
-    response = client.post('/api/employee/{}/'.format(temporary_employee.id),
+    response = client.post('/employee/{}/'.format(temporary_employee.id),
                            json.dumps({"name": "Funcionario Alterado", "email": "email@email.com", "department": "TI"}),
                            HTTP_X_METHOD_OVERRIDE='PUT',
                            content_type='application/json')
@@ -56,7 +67,7 @@ def test_update(client, temporary_employee):
 def test_delete(client, temporary_employee):
     from api.models import Employee
 
-    response = client.post('/api/employee/{}/'.format(temporary_employee.id),
+    response = client.post('/employee/{}/'.format(temporary_employee.id),
                            json.dumps({"id": temporary_employee.id}),
                            HTTP_X_METHOD_OVERRIDE='DELETE',
                            content_type='application/json')
@@ -68,11 +79,11 @@ def test_delete(client, temporary_employee):
 
 @pytest.mark.django_db
 def test_get(client, temporary_employee):
-    response = client.get('/api/employee/{}/'.format(temporary_employee.id))
+    response = client.get('/employee/{}/'.format(temporary_employee.id))
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_get_not_found(client, temporary_employee):
-    response = client.get('/api/employee/46545456/')
+    response = client.get('/employee/46545456/')
     assert response.status_code == 404
