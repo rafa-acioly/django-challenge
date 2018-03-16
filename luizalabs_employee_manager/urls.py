56a258ddd77085facabfd52bf4284c663302d268
Rafael Acioly <aciolyr@gmail.com>
2018-03-16 17:02:17 -0300

    Inclui view na URL principal
    Adiciona uma pequena pagina em HTML para a rota principal.

--- luizalabs_employee_manager/urls.py
+++ luizalabs_employee_manager/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
 from api.models import Employee
 import viewsets
+from api.views import home
 
 router = routers.DefaultRouter()
 router.register(r'employee', viewsets.EmployeeViewSet)
 
 urlpatterns = [
+    url(r'^$', home, name="home"),
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
 ]
